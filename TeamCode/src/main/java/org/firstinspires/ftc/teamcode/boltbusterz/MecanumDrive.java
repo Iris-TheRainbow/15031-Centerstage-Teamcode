fb03d021fdafc5a181b48e51ea509352efd15d5c
Iris <87090296+Iris-TheRainbow@users.noreply.github.com>
2024-01-06 22:18:13 -0600

    The power of 2 (sticks)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/boltbusterz/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/boltbusterz/MecanumDrive.java
@@ -1,30 +1,65 @@
 package org.firstinspires.ftc.teamcode.boltbusterz;
 
+import com.arcrobotics.ftclib.controller.PIDController;
+
 import java.util.Arrays;
 
 @SuppressWarnings("unused")
 public class MecanumDrive {
     public double throttle;
-    public MecanumDrive(double throttle){ this.throttle = throttle; }
+    public PIDController headingController;
+    public double p, i, d;
+    public MecanumDrive(double throttle){
+        this.throttle = throttle;
+        headingController = new PIDController(p, i, d);
+    }
+    double frPower, brPower, blPower, flPower;
+    double sin, cos, power, max, direction, targetHeading, turn;
     public double[] calculateTankPower(double rightStrafe, double leftStrafe, double leftTank, double rightTank){
         double[] inputs = {throttle * rightStrafe, throttle * leftStrafe, throttle * leftTank, throttle * rightTank, 1};
         double normalizer = Arrays.stream(inputs).max().getAsDouble();
-        double frPower = (rightTank - rightStrafe + leftStrafe) * throttle / normalizer;
-        double brPower = (rightTank + rightStrafe - leftStrafe) * throttle / normalizer;
-        double blPower = (leftTank + leftStrafe - rightStrafe) * throttle / normalizer;
-        double flPower = (leftTank - leftStrafe + rightStrafe) * throttle / normalizer;
+        frPower = (rightTank - rightStrafe + leftStrafe) * throttle / normalizer;
+        brPower = (rightTank + rightStrafe - leftStrafe) * throttle / normalizer;
+        blPower = (leftTank + leftStrafe - rightStrafe) * throttle / normalizer;
+        flPower = (leftTank - leftStrafe + rightStrafe) * throttle / normalizer;
         return new double[] {frPower, brPower, blPower, flPower};
     }
     public  double[] calculateOneStickPower(double x, double y, double turn, double offset){
-        double direction = Math.atan2(y, x) - Math.toRadians(offset);
-        double sin = Math.sin(direction - Math.PI/4);
-        double cos = Math.sin(direction - Math.PI/4);
-        double power = Math.sqrt(x*x + y*y);
-        double max = Math.max(Math.abs(sin), Math.abs(cos));
-        double frPower = power * sin/max + turn;
-        double brPower = power * cos/max + turn;
-        double blPower = power * sin/max + turn;
-        double flPower = power * cos/max + turn;
+        direction = Math.atan2(y, x) - Math.toRadians(offset);
+        sin = Math.sin(direction - Math.PI/4);
+        cos = Math.sin(direction - Math.PI/4);
+        power = Math.sqrt(x*x + y*y);
+        max = Math.max(Math.abs(sin), Math.abs(cos));
+        frPower = power * sin/max - turn;
+        brPower = power * cos/max - turn;
+        blPower = power * sin/max + turn;
+        flPower = power * cos/max + turn;
+        if ((power + Math.abs(turn)) > 1){
+            frPower /= power + Math.abs(turn);
+            brPower /= power + Math.abs(turn);
+            blPower /= power + Math.abs(turn);
+            flPower /= power + Math.abs(turn);
+        }
+        return new double[] {frPower, brPower, blPower, flPower};
+    }
+
+    public double[] calculateOneStickPidHeadingPower(double driveX, double drvieY, double headingX, double headingY, double currentHeading){
+        direction = Math.atan2(drvieY, driveX) - Math.toRadians(currentHeading);
+        sin = Math.sin(direction - Math.PI/4);
+        cos = Math.sin(direction - Math.PI/4);
+        power = Math.sqrt(driveX*driveX + drvieY*drvieY);
+        max = Math.max(Math.abs(sin), Math.abs(cos));
+        targetHeading = currentHeading - Math.toDegrees(Math.atan2(headingY, headingX));
+        if (targetHeading > 180) {
+            targetHeading = (-360) + targetHeading;
+        } else if (targetHeading < -180) {
+            targetHeading= 360 + targetHeading;
+        }
+        turn = headingController.calculate(currentHeading, targetHeading);
+        frPower = power * sin/max - turn;
+        brPower = power * cos/max - turn;
+        blPower = power * sin/max + turn;
+        flPower = power * cos/max + turn;
         if ((power + Math.abs(turn)) > 1){
             frPower /= power + Math.abs(turn);
             brPower /= power + Math.abs(turn);
